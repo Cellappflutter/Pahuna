8c52f0a12663f5116176366f19b19016d7bb386a
mysticalfluffyman <rajbhandaripratik8@gmail.com>
2020-02-26 15:58:44 +0545

    name

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ScreenSizeConfig().init(context);
+    print("pratik");
     return MaterialApp(
       title: 'Pahuna',
       initialRoute: '/',
