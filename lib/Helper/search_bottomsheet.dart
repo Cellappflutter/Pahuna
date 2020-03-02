91ce22fe889654346c9fcc73d78492d3462c602a
pratik <testing.firebase99@gmail.com>
2020-03-02 14:33:56 +0545

    search_bottombar_view

--- lib/Helper/search_bottomsheet.dart
+++ lib/Helper/search_bottomsheet.dart
@@ -1,84 +1,78 @@
+import 'package:ecommerce_app_ui_kit/config/ui_icons.dart';
 import 'package:ecommerce_app_ui_kit/database/database.dart';
 import 'package:flutter/material.dart';
-
+import 'package:ecommerce_app_ui_kit/config/app_config.dart' as config;
 import 'UiHelper/loading.dart';
 
+class Search_BottomSheet extends StatefulWidget {
+  String name, email;
+  Search_BottomSheet(this.email, this.name);
 
-class Search_BottomSheet extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _searchsheet();
-      }
-    }
-    
-    class _searchsheet extends State<Search_BottomSheet>{
-      @override
+  }
+}
+
+class _searchsheet extends State<Search_BottomSheet> {
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-                      decoration: BoxDecoration(boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 10)
-                      ]),
-                      margin: EdgeInsets.only(top: 30),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              height: 200,
-                              width: MediaQuery.of(context).size.width,
-                              color: Colors.red,
-                              child: CircleAvatar(),
-                            ),
-                            Container(
-                              height: 200,
-                              width: MediaQuery.of(context).size.width,
-                              color: Colors.green,
-                              child: Column(
-                                children: <Widget>[
-                                 // Text(info[i].uid),
-                                  Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: <Widget>[
-                                        RaisedButton(
-                                          child: Text('Send Request'),
-                                          onPressed: () {
-                                            final pr = loadingBar(
-                                                context, "Sending Request");
-                                            pr.show();
-                                            DatabaseService()
-                                                .sendReq(null)//info[i].uid)
-                                                .then((onValue) {
-                                              pr.dismiss();
-                                            });
-                                          },
-                                        ),
-                                        SizedBox(width: 10.0),
-                                        RaisedButton(
-                                          child: Text('Follow'),
-                                          onPressed: () {},
-                                        )
-                                      ]),
-                                ],
-                              ),
-                            ),
-                            Container(
-                              height: 200,
-                              color: Colors.blue,
-                            ),
-                            Container(
-                              height: 200,
-                              color: Colors.yellow,
-                            ),
-                          ],
+      height: 400,
+      child: Column(
+        
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.center,
+                  child: CircleAvatar(
+                    maxRadius: 60,
+                    backgroundImage: AssetImage("img/user1.jpg"),
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+               // Align(
+                //  alignment: Alignment.center,
+                 // child: 
+                  Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[ Text(widget.name,style: Theme.of(context).textTheme.body2)],
+                      ),
+                      
+                      SizedBox(height: 15,),
+                     
+                       Row(
+                       // mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[Text("Email : ",style: Theme.of(context).textTheme.body2,), Text(widget.email,style:Theme.of(context).textTheme.body1 ,)],
+                      ),
+                      SizedBox(height: 15,),
+                      Container(
+                        child: RichText(text: TextSpan(text: "Description : ",style: Theme.of(context).textTheme.body2,children: [TextSpan(
+                          text: "This is blah blah basdfjbasdfhue asdjkfkajsd sdhfjkahsdjf jhsdfjasd jshfjasd sdfasdfds asfadsfasdf asdfasd asdfasf asdfasdf adsf adsfadsf ",style: Theme.of(context).textTheme.body1,
+                        )]),
                         ),
                       ),
-                    );;
+                      SizedBox(height: 25,),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: RaisedButton.icon(onPressed: ()=>print("send request"), icon: Icon(Icons.check), label: Text("Send Request")))
+                    ],
+                  ),
+                //)
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
