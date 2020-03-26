bb68b02e322d4783eb81689d30df47d95675ce70
Rishan <testing.firebase99@gmail.com>
2020-03-26 18:48:50 +0545

    done march 26

--- lib/database/Word.dart
+++ lib/database/Word.dart
@@ -7,22 +7,20 @@ class Wordget {
   WordPressAPI api = WordPressAPI('http://cellapp.info/demo/crm/wp-json/wp/v2');
 
   Future<List<Featuredata>> word() async {
-    // try {
-    //   final posts = (await api.getAsync('posts').catchError((onError) {
-    //     return [];
-    //   }))['data'];
-    //   List<Featuredata> data = List<Featuredata>();
-    //   for (final post in posts) {
-    //     Featuredata data1 = Featuredata(
-    //         title: post['title']['rendered'],
-    //         image: post['jetpack_featured_media_url'],
-    //         content: post['content']['rendered'],
-    //         id: post['id'].toString());
-    //     data.add(data1);
-    //   }
-    //   return data;
-   // } catch (e) {
+    try {
+      final posts = (await api.getAsync('posts'))['data'];
+      List<Featuredata> data = List<Featuredata>();
+      for (final post in posts) {
+        Featuredata data1 = Featuredata(
+            title: post['title']['rendered'],
+            image: post['jetpack_featured_media_url'],
+            content: post['content']['rendered'],
+            id: post['id'].toString());
+        data.add(data1);
+      }
+      return data;
+   } catch (e) {
       return [];
-  //  }
+   }
   }
 }
